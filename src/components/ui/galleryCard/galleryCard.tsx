cb5dc461f6f7fa969cbfc42dcf34ec5ebe05bcff
Rafelvdy <rafe.loveday@icloud.com>
2025-07-16 12:31:25 +0100

    gallery card animation triggered moved up page

--- src/components/ui/galleryCard/galleryCard.tsx
+++ src/components/ui/galleryCard/galleryCard.tsx
@@ -20,8 +20,8 @@ const GalleryCard = ({ index }: GalleryCardProps) => {
         const animation = gsap.to(galleryCardRef.current, {
             scrollTrigger: {
                 trigger: galleryCardRef.current,
-                start: "top 90%",
-                end: "top 60%",
+                start: "top 80%",
+                end: "top 50%",
                 scrub: true,
                 markers: false,
             },
